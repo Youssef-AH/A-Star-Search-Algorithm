2a1d69bef3914e447748f8c0cd87fc5a2fc047a5
Void <qpinoza@gmail.com>
2019-07-11 14:50:31 +0100

    small fix

--- src/AStarSearch.java
+++ src/AStarSearch.java
@@ -9,14 +9,14 @@ public class AStarSearch {
 
     private PriorityQueue<Node> openNodes; // nodes to be examined
     private boolean[][] closedNodes; // nodes already examined
-    private int[][] grid; //
+    private Node[][] grid;
 
-    public AStarSearch(int[][] grid, Node startNode, Node targetNode) {
+    public AStarSearch(Node startNode, Node targetNode, int width, int height, int[][] blocks) {
 
-        this.grid = grid;
         this.startNode = startNode;
         this.targetNode = targetNode;
 
+        this.closedNodes = new boolean[width][height];
         this.openNodes = new PriorityQueue<>(new Comparator<Node>() {
             @Override
             public int compare(Node node1, Node node2) {
